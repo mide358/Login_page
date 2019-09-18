370e348083220a4c11bb624e141148efa90a8652
Ennyola <medunoyeeni@gmail.com>
2019-09-18 15:20:52 +0100

    Made login.html perform the login authentication

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
     
      if(isset($_POST['email'])){
 
-        echo "udnidnoinonod";
 
         $email = $_POST['email'];
         $password = $_POST['psw'];
@@ -33,7 +32,7 @@
             }
             
             else{
-                echo "Invalid Login credentials";
+                echo "<h1 align = 'center' style = ' color : purple;'>Invalid Login credentials</h1>";
             }
     
         
@@ -47,40 +46,3 @@
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link type="text/css" rel="stylesheet" href="main.css">
-        <title>Login Page</title>
-    </head>
-    <body>
-       <!--login form details-->
-        <form method = "POST" action="#">
-            <!--logo for the page-->
-            <div class="img">
-                <img src="https://res.cloudinary.com/mide358/image/upload/v1568667134/logo_rrxeuy.png" alt="logo">
-            </div>
-
-            <div class="form-container">
-                <!--email with set input format -->
-                <input type="email" id="email" name="email" placeholder="email" required 
-                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/><br><br>
-
-
-                <input type="password" id="password" name="psw" placeholder = "Password"  required><br><br>
-
-                <button class="button" type="submit" name = "submit">LOG IN</button>
-                        
-            </div>
-            
-        </form>
-        <div class="other-details">
-            <!--The href tags from sign up/forgot password has not been inserted-->
-            <a href=" ">Don't have an account?</a>   <a href= " ">Home </a>        
-                       
-        </div>
-        
-    </body>
-</html>
